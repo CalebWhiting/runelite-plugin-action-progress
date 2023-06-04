423157ea1cb671482e943b4611c1a4f3c070c335
Krytlex <boudreauguillaume09@gmail.com>
2023-06-04 10:06:23 -0400

    Remove javax annotation

--- src/main/java/com/github/calebwhiting/runelite/data/Crafting.java
+++ src/main/java/com/github/calebwhiting/runelite/data/Crafting.java
@@ -2,12 +2,10 @@ package com.github.calebwhiting.runelite.data;
 
 import net.runelite.api.ItemID;
 
-import javax.annotation.Generated;
-
 public interface Crafting
 {
 
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] SILVER_AND_GOLD_ITEMS = {
+	int[] SILVER_AND_GOLD_ITEMS = {
 			ItemID.GOLD_RING, ItemID.SAPPHIRE_RING, ItemID.EMERALD_RING, ItemID.RUBY_RING, ItemID.DIAMOND_RING,
 			ItemID.DRAGONSTONE_RING, ItemID.GOLD_NECKLACE, ItemID.SAPPHIRE_NECKLACE, ItemID.EMERALD_NECKLACE,
 			ItemID.RUBY_NECKLACE, ItemID.DIAMOND_NECKLACE, ItemID.DRAGON_NECKLACE, ItemID.GOLD_AMULET_U,
@@ -25,34 +23,34 @@ public interface Crafting
 			ItemID.OPAL_AMULET_U, ItemID.JADE_AMULET_U, ItemID.TOPAZ_AMULET_U, ItemID.OPAL_BRACELET,
 			ItemID.JADE_BRACELET, ItemID.TOPAZ_BRACELET
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] SAPPHIRE_AND_OPAL_JEWELLERY = {
+	int[] SAPPHIRE_AND_OPAL_JEWELLERY = {
 			ItemID.SAPPHIRE_RING, ItemID.SAPPHIRE_NECKLACE, ItemID.SAPPHIRE_AMULET, ItemID.SAPPHIRE_BRACELET,
 			ItemID.SAPPHIRE_BRACELET_11072, ItemID.OPAL_RING, ItemID.OPAL_NECKLACE, ItemID.OPAL_AMULET,
 			ItemID.OPAL_BRACELET
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] EMERALD_JEWELLERY = {
+	int[] EMERALD_JEWELLERY = {
 			ItemID.EMERALD_RING, ItemID.EMERALD_NECKLACE, ItemID.EMERALD_AMULET, ItemID.PRENATURE_AMULET,
 			ItemID.EMERALD_BRACELET, ItemID.EMERALD_BRACELET_11078, ItemID.JADE_RING, ItemID.JADE_NECKLACE,
 			ItemID.JADE_AMULET, ItemID.JADE_BRACELET
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] TOPAZ_AND_RUBY_JEWELLERY = {
+	int[] TOPAZ_AND_RUBY_JEWELLERY = {
 			ItemID.RUBY_RING, ItemID.RUBY_NECKLACE, ItemID.RUBY_AMULET, ItemID.RUBY_BRACELET,
 			ItemID.RUBY_BRACELET_11087, ItemID.TOPAZ_RING, ItemID.TOPAZ_NECKLACE, ItemID.TOPAZ_AMULET,
 			ItemID.TOPAZ_BRACELET
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] DIAMOND_JEWELLERY = {
+	int[] DIAMOND_JEWELLERY = {
 			ItemID.DIAMOND_RING, ItemID.DIAMOND_NECKLACE, ItemID.DIAMOND_AMULET, ItemID.DIAMOND_BRACELET,
 			ItemID.DIAMOND_BRACELET_11094
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] DRAGONSTONE_JEWELLERY = {
+	int[] DRAGONSTONE_JEWELLERY = {
 			ItemID.DRAGONSTONE_RING, ItemID.DRAGON_NECKLACE, ItemID.DRAGONSTONE_AMULET, ItemID.DRAGONSTONE_BRACELET,
 			ItemID.DRAGON_BRACELET
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] ONYX_JEWELLERY = {
+	int[] ONYX_JEWELLERY = {
 			ItemID.ONYX_RING, ItemID.ONYX_NECKLACE, ItemID.ONYX_AMULET, ItemID.ONYX_BRACELET,
 			ItemID.ONYX_BRACELET_11132
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] ZENYTE_JEWELLERY = {
+	int[] ZENYTE_JEWELLERY = {
 			ItemID.ZENYTE_BRACELET, ItemID.ZENYTE_BRACELET_19532, ItemID.ZENYTE_NECKLACE, ItemID.ZENYTE_RING,
 			ItemID.ZENYTE_AMULET
 	};
--- src/main/java/com/github/calebwhiting/runelite/data/Fletching.java
+++ src/main/java/com/github/calebwhiting/runelite/data/Fletching.java
@@ -2,12 +2,10 @@ package com.github.calebwhiting.runelite.data;
 
 import net.runelite.api.ItemID;
 
-import javax.annotation.Generated;
-
 public interface Fletching
 {
 
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] UNENCHANTED_BOLTS_AND_ARROWS = {
+	int[] UNENCHANTED_BOLTS_AND_ARROWS = {
 			ItemID.BRONZE_BOLTS, ItemID.OPAL_BOLTS, ItemID.PEARL_BOLTS, ItemID.BARBED_BOLTS, ItemID.BRONZE_ARROW,
 			ItemID.IRON_ARROW, ItemID.STEEL_ARROW, ItemID.MITHRIL_ARROW, ItemID.ADAMANT_ARROW, ItemID.RUNE_ARROW,
 			ItemID.OGRE_ARROW, ItemID.BROAD_ARROWS, ItemID.BROAD_ARROWS_4160, ItemID.BRONZE_BRUTAL, ItemID.IRON_BRUTAL,
@@ -20,12 +18,12 @@ public interface Fletching
 			ItemID.BROAD_BOLTS, ItemID.ADAMANT_ARROW_20388, ItemID.DRAGON_ARROW_20389, ItemID.RUNE_ARROW_20600,
 			ItemID.AMETHYST_ARROW, ItemID.DRAGON_BOLTS, ItemID.BARBED_ARROW
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] BOLT_TIPS = {
+	int[] BOLT_TIPS = {
 			ItemID.OPAL_BOLT_TIPS, ItemID.PEARL_BOLT_TIPS, ItemID.JADE_BOLT_TIPS, ItemID.TOPAZ_BOLT_TIPS,
 			ItemID.SAPPHIRE_BOLT_TIPS, ItemID.EMERALD_BOLT_TIPS, ItemID.RUBY_BOLT_TIPS, ItemID.DIAMOND_BOLT_TIPS,
 			ItemID.DRAGONSTONE_BOLT_TIPS, ItemID.ONYX_BOLT_TIPS, ItemID.AMETHYST_BOLT_TIPS
 	};
-	@Generated("com/github/calebwhiting/runelite/data/DataVerify") int[] ENCHANTED_BOLTS = {
+	int[] ENCHANTED_BOLTS = {
 			ItemID.OPAL_BOLTS_E, ItemID.JADE_BOLTS_E, ItemID.PEARL_BOLTS_E, ItemID.TOPAZ_BOLTS_E,
 			ItemID.SAPPHIRE_BOLTS_E, ItemID.EMERALD_BOLTS_E, ItemID.RUBY_BOLTS_E, ItemID.DIAMOND_BOLTS_E,
 			ItemID.DRAGONSTONE_BOLTS_E, ItemID.ONYX_BOLTS_E, ItemID.OPAL_DRAGON_BOLTS_E, ItemID.JADE_DRAGON_BOLTS_E,
